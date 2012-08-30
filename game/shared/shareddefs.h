2efbe23f8d1509da5c826a3f9ca767dabe53e3ce
Drifter <drifter01620@gmail.com>
2012-08-30 08:08:11 -0500

    Added missing HIDEHUD_RADAR to sharedefs (bug 5464, r=ds).

--- game/shared/shareddefs.h
+++ game/shared/shareddefs.h
@@ -149,8 +149,9 @@ inline float FractionUnDucked( int msecs )
 #define	HIDEHUD_VEHICLE_CROSSHAIR	( 1<<9 )	// Hide vehicle crosshair
 #define HIDEHUD_INVEHICLE			( 1<<10 )
 #define HIDEHUD_BONUS_PROGRESS		( 1<<11 )	// Hide bonus progress display (for bonus map challenges)
+#define HIDEHUD_RADAR				( 1<<12 )	// Hide the radar
 
-#define HIDEHUD_BITCOUNT			12
+#define HIDEHUD_BITCOUNT			13
 
 //===================================================================================================================
 // suit usage bits
