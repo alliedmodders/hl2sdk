c0d4f54a2a922045c83e94ba45f1b308d4f2cb23
Nicholas Hastings <skamonkey@gmail.com>
2013-03-21 21:13:05 -0400

    Updated ICommandLine.

--- public/tier0/icommandline.h
+++ public/tier0/icommandline.h
@@ -25,19 +25,21 @@ public:
 
 	// Check whether a particular parameter exists
 	virtual	const char	*CheckParm( const char *psz, const char **ppszValue = 0 ) const = 0;
+	virtual bool		HasParm( const char *psz ) const = 0;
 	virtual void		RemoveParm( const char *parm ) = 0;
 	virtual void		AppendParm( const char *pszParm, const char *pszValues ) = 0;
+	
+	// Gets at particular parameters
+	virtual int			ParmCount() const = 0;
+	virtual int			FindParm( const char *psz ) const = 0;	// Returns 0 if not found.
+	virtual const char* GetParm( int nIndex ) const = 0;
 
 	// Returns the argument after the one specified, or the default if not found
 	virtual const char	*ParmValue( const char *psz, const char *pDefaultVal = 0 ) const = 0;
 	virtual int			ParmValue( const char *psz, int nDefaultVal ) const = 0;
 	virtual float		ParmValue( const char *psz, float flDefaultVal ) const = 0;
+	virtual bool		ParmValue( const char *psz, const char *pDefaultVal, char *pszOut, unsigned int size ) = 0;
 
-	// Gets at particular parameters
-	virtual int			ParmCount() const = 0;
-	virtual int			FindParm( const char *psz ) const = 0;	// Returns 0 if not found.
-	virtual const char* GetParm( int nIndex ) const = 0;
-	
 	// copies the string passwed
 	virtual void SetParm( int nIndex, char const *pNewParm ) =0;
 };
