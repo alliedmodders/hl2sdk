a111d890dad84d23135054b92fede1f69a168aca
Nicholas Hastings <nshastings@gmail.com>
2013-11-03 09:37:09 -0500

    Undefine RESTRICT if GNUC due to ABI incompat between GCC and Clang.

--- public/tier0/platform.h
+++ public/tier0/platform.h
@@ -500,11 +500,7 @@ typedef void * HINSTANCE;
 	#define FMTFUNCTION( a, b )
 #elif defined(GNUC)
 	#define SELECTANY __attribute__((weak))
-	#if defined(LINUX) && !defined(DEDICATED)
-		#define RESTRICT
-	#else
-		#define RESTRICT __restrict
-	#endif
+	#define RESTRICT
 	#define RESTRICT_FUNC
 	// squirrel.h does a #define printf DevMsg which leads to warnings when we try
 	// to use printf as the prototype format function. Using __printf__ instead.
