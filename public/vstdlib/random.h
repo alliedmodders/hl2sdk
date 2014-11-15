a4c483063e97f4cea103606183c18c39de4fe371
Nicholas Hastings <nshastings@gmail.com>
2014-11-15 11:32:47 -0500

    Fix new warning from cset 5474d11ed00efe.

--- public/vstdlib/random.h
+++ public/vstdlib/random.h
@@ -24,7 +24,7 @@
 //-----------------------------------------------------------------------------
 // A generator of uniformly distributed random numbers
 //-----------------------------------------------------------------------------
-class IUniformRandomStream
+class VSTDLIB_CLASS IUniformRandomStream
 {
 public:
 	virtual ~IUniformRandomStream() {}
