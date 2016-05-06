a26894ca45f66fd6a1f5faa80db70c34fff0f634
Robin Gohmert <ayuto@web.de>
2016-05-06 22:09:10 +0200

    Fixed an include statement (#24)

--- public/avi/iavi.h
+++ public/avi/iavi.h
@@ -14,7 +14,7 @@
 #pragma once
 #endif
 
-#include "appframework/iappsystem.h"
+#include "appframework/IAppSystem.h"
 
 //-----------------------------------------------------------------------------
 // Forward declarations
