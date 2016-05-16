f4bfdec7a36e1c8a90200b19bd1ace98febe85cb
Robin Gohmert <ayuto@web.de>
2016-05-16 23:17:56 +0200

    Updated MAX_EDICT_BITS constant (#25)

--- public/const.h
+++ public/const.h
@@ -54,7 +54,7 @@
 #define SP_MODEL_INDEX_BITS			13
 
 // How many bits to use to encode an edict.
-#define	MAX_EDICT_BITS				11			// # of bits needed to represent max edicts
+#define	MAX_EDICT_BITS				12			// # of bits needed to represent max edicts
 // Max # of edicts in a level
 #define	MAX_EDICTS					(1<<MAX_EDICT_BITS)
 
