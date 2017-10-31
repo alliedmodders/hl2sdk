277495c0124f248f8502466a522954aba3ca035c
Nicholas Hastings <nshastings@gmail.com>
2017-10-31 17:20:45 -0400

    Revert "Add missing file from last commit."
    This reverts commit 6b111017098c8958ebcee56a0178d2a1425d506f.

--- public/filesystem_passthru.h
+++ public/filesystem_passthru.h
@@ -95,7 +95,6 @@ public:
 	virtual const AppSystemInfo_t* GetDependencies()															{ return m_pFileSystemPassThru->GetDependencies(); }
 	virtual AppSystemTier_t GetTier()																			{ return m_pFileSystemPassThru->GetTier(); }
 	virtual void Reconnect( CreateInterfaceFn factory, const char *pInterfaceName )								{ m_pFileSystemPassThru->Reconnect( factory, pInterfaceName ); }
-	virtual bool IsSingleton()																					{ return m_pFileSystemPassThru->IsSingleton(); }
 
 	virtual void			RemoveAllSearchPaths( void )														{ m_pFileSystemPassThru->RemoveAllSearchPaths(); }
 	virtual void			AddSearchPath( const char *pPath, const char *pathID, SearchPathAdd_t addType )		{ m_pFileSystemPassThru->AddSearchPath( pPath, pathID, addType ); }
