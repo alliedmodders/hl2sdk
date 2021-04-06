a0b40b69393b4a35ac2038c14f3ba52f356815e4
arthurdead <arthur.kinder@gmail.com>
2021-04-06 14:45:10 -0300

    add missing vtable

--- public/ispatialpartition.h
+++ public/ispatialpartition.h
@@ -112,6 +112,8 @@ enum
 abstract_class ISpatialPartition
 {
 public:
+	virtual ~ISpatialPartition() = 0;
+	
 	// Create/destroy a handle for this dude in our system. Destroy
 	// will also remove it from all lists it happens to be in
 	virtual SpatialPartitionHandle_t CreateHandle( IHandleEntity *pHandleEntity ) = 0;
