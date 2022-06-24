a5dbb62fd6211f2cd297635855660b0040663977
PerfectLaugh <20559490+PerfectLaugh@users.noreply.github.com>
2022-06-24 21:23:27 +0800

    KeyValues update on 6/24/2022 (#102)
    * to get around gcc compilation
    
    * KeyValues update on 6/24/2022
    
    * I should initialize the additional varaibles
    
    * correct vtable destructor
    
    * remove unknown1 virtual

--- public/tier1/KeyValues.h
+++ public/tier1/KeyValues.h
@@ -342,6 +342,9 @@ private:
 	char	   m_bHasEscapeSequences; // true, if while parsing this KeyValue, Escape Sequences are used (default false)
 	uint16	   m_iKeyNameCaseSensitive2;	// 2nd part of case sensitive symbol defined in KeyValueSystem;
 
+	uint32     m_iUnk1;
+	uint32     m_iUnk2;
+
 	KeyValues *m_pPeer;	// pointer to next key in list
 	KeyValues *m_pSub;	// pointer to Start of a new sub key list
 	KeyValues *m_pChain;// Search here if it's not in our list
