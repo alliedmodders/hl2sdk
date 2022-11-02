b3addd57ec0304e03b8a52a5f1f869609538df0a
SirWillian <wsimanbrazil@yahoo.com.br>
2022-11-01 21:44:01 -0300

    Fixed tier2 and tier3 not compiling under C++20 (#112)

--- public/tier2/tier2.h
+++ public/tier2/tier2.h
@@ -1,4 +1,4 @@
-//===== Copyright © 2005-2005, Valve Corporation, All rights reserved. ======//
+//===== Copyright Â© 2005-2005, Valve Corporation, All rights reserved. ======//
 //
 // Purpose: A higher level link library for general use in the game and tools.
 //
@@ -84,7 +84,7 @@ public:
 		if ( !BaseClass::Connect( factory ) )
 			return false;
 
-		if ( IsPrimaryAppSystem() )
+		if ( BaseClass::IsPrimaryAppSystem() )
 		{
 			ConnectTier2Libraries( &factory, 1 );
 		}
@@ -93,7 +93,7 @@ public:
 
 	virtual void Disconnect() 
 	{
-		if ( IsPrimaryAppSystem() )
+		if ( BaseClass::IsPrimaryAppSystem() )
 		{
 			DisconnectTier2Libraries();
 		}
