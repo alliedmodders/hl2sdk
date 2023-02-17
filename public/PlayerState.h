6f8b684753626bbedc463f2ed5a382ff54e5de3d
Rafał <rafradek@hotmail.com>
2023-02-17 01:34:49 +0100

    Add missing replay bool in CPlayerState from sdk2013 to tf2 branch (#124)
    (cherry picked from commit 78673d4c4aac783f30c80491997b22a9f6281309)

--- public/PlayerState.h
+++ public/PlayerState.h
@@ -1,4 +1,4 @@
-//========= Copyright © 1996-2005, Valve Corporation, All rights reserved. ============//
+//========= Copyright Â© 1996-2005, Valve Corporation, All rights reserved. ============//
 //
 // Purpose: 
 //
@@ -42,8 +42,9 @@ public:
 	int			fixangle;
 	// delta angle for fixangle == FIXANGLE_RELATIVE
 	QAngle		anglechange;
-	// flag to single the HLTV fake client, not transmitted
+	// flag to single the HLTV/Replay fake client, not transmitted
 	bool		hltv;
+	bool		replay;
 	int			frags;
 	int			deaths;
 #endif
