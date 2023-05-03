225e405ef4cdb1d605ecc9b521a37ada4dc4ea88
GAMMACASE <darknesss456@mail.ru>
2023-05-03 08:57:49 +0300

    Update IGameEventSystem & INetworkMessages interfaces
    Also adds IProtobufBinding & INetworkSerializable interfaces and CNetworkSerializerPB class

---
+++ public/networksystem/inetworkserializer.h
@@ -0,0 +1,69 @@
+#ifndef NETWORKSERIALIZER_H
+#define NETWORKSERIALIZER_H
+
+#ifdef _WIN32
+#pragma once
+#endif
+
+#include <platform.h>
+
+#include <networksystem/iprotobufbinding.h>
+#include <tier1/utlstring.h>
+#include <tier1/bitbuf.h>
+#include "Color.h"
+
+enum NetworkValidationMode_t;
+enum NetworkSerializationMode_t;
+
+typedef uint16 NetworkMessageId;
+typedef uint8 NetworkGroupId;
+typedef uint NetworkCategoryId;
+
+struct NetMessageInfo_t
+{
+	int m_nCategories;
+	IProtobufBinding *m_pBinding;
+	CUtlString m_szGroup;
+	NetworkMessageId m_MessageId;
+	NetworkGroupId m_GroupId;
+
+	// (1 << 0) - FLAG_RELIABLE
+	// (1 << 6) - FLAG_AUTOASSIGNEDID
+	// (1 << 7) - FLAG_UNK002
+	uint8 m_nFlags;
+
+	int m_unk001;
+	int m_unk002;
+	bool m_bOkayToRedispatch;
+};
+
+abstract_class INetworkSerializable
+{
+public:
+	virtual ~INetworkSerializable() = 0;
+
+	virtual const char *GetUnscopedName() = 0;
+	virtual NetMessageInfo_t *GetNetMessageInfo() = 0;
+
+	virtual void SetMessageId(unsigned short nMessageId) = 0;
+
+	virtual void AddCategoryMask(int nMask, bool) = 0;
+
+	virtual void SwitchMode(NetworkValidationMode_t nMode) = 0;
+
+	virtual void *AllocateMessage() = 0;
+	virtual void DeallocateMessage(void *pMsg) = 0;
+	virtual void *AllocateAndCopyConstructNetMessage(void const *pOther) = 0;
+
+	virtual bool Serialize(bf_write &pBuf, void const *pData, NetworkSerializationMode_t unused) = 0;
+	virtual bool Unserialize(bf_read &pBuf, void *pData, NetworkSerializationMode_t unused) = 0;
+};
+
+class CNetworkSerializerPB : public INetworkSerializable
+{
+private:
+	const char *m_szUnscopedName;
+	NetMessageInfo_t m_MessageInfo;
+};
+
+#endif /* NETWORKSERIALIZER_H */
---
+++ public/networksystem/iprotobufbinding.h
@@ -0,0 +1,38 @@
+#ifndef PROTOBUFBINDING_H
+#define PROTOBUFBINDING_H
+
+#ifdef _WIN32
+#pragma once
+#endif
+
+#include <tier1/utlstring.h>
+#include <tier1/bitbuf.h>
+#include "Color.h"
+
+enum NetChannelBufType_t;
+
+abstract_class IProtobufBinding
+{
+public:
+	virtual const char *GetName() = 0;
+	virtual int GetSize() = 0;
+
+	virtual const char *ToString(const void *pData, CUtlString &sResult) = 0;
+
+	virtual const char *GetGroup() = 0;
+	virtual Color GetGroupColor() = 0;
+	virtual NetChannelBufType_t GetBufType() = 0;
+
+	virtual bool ReadFromBuffer(void *pData, bf_read &pBuf) = 0;
+	virtual bool WriteToBuffer(const void *pData, bf_write &pBuf) = 0;
+
+	virtual void *AllocateMessage() = 0;
+	virtual void DeallocateMessage(void *pMsg) = 0;
+	virtual void *AllocateAndCopyConstructNetMessage(const void *pOther) = 0;
+
+	virtual bool OkToRedispatch() = 0;
+	virtual void Copy(const void *pFrom, void *pTo) = 0;
+	virtual bool unk001() = 0;
+};
+
+#endif /* PROTOBUFBINDING_H */
