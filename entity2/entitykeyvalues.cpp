5b83ab87279c1b0012a1127ed4a8a9007620e06f
karl-police <karl-police2001@bluewin.ch>
2023-10-01 20:59:28 +0200

    Update entitykeyvalues.cpp

--- entity2/entitykeyvalues.cpp
+++ entity2/entitykeyvalues.cpp
@@ -1,38 +1,34 @@
 #include "entity2/entitysystem.h"
 
-void CEntityKeyValues::ReleaseAllComplexKeys() {
-  CEntityKeyValues::EntityComplexKeyListElem_t *i;
-  IEntityKeyComplex *m_pKey;
-  int m_nRef;
-
-  for ( i = this->m_pComplexKeys; i; i = i->m_pNext ) {
-      while(1) { // ehhh...???
-        m_pKey = i->m_pKey;
-        m_nRef = i->m_pKey->m_nRef;
-        i->m_pKey->m_nRef = m_nRef - 1;
-        if ( m_nRef == 1 )
-          break;
-        i = i->m_pNext;
-        if ( !i )
-          this->m_pComplexKeys = 0LL;
-      }
-      (*m_pKey->_vptr_IEntityKeyComplex)(m_pKey);
+void CEntityKeyValues::ReleaseAllComplexKeys()
+{    
+    for ( EntityComplexKeyListElem_t* i = m_pComplexKeys; i; i = i->m_pNext )
+    {
+        IEntityKeyComplex* pKey = i->m_pKey;
+        while (pKey->m_nRef-- != 1)
+        {
+            i = i->m_pNext;            
+            if (!i)
+                m_pComplexKeys = nullptr; // 0LL      
+            
+            pKey = i->m_pKey; // (*m_pKey->_vptr_IEntityKeyComplex)(m_pKey)
+        }
+        
+        pKey->vptr();
     }
-  }
 }
 
 
+
 void CEntityKeyValues::RemoveAllKeys() {
-  CUtlLeanVectorFixedGrowableBase<unsigned int,9u,short int>::IndexType_t m_nAllocated;
-  CUtlLeanVectorBase<CEntityKeyValues::KeyValueInfo_t,short int>::ElemType_t *m_pElements;
-  uint16 m_flags;
+    
 }
 
 // CEntityKeyValues::Serialize
 
 
-__int64 CEntityKeyValues::Unserialize(CUtlBuffer *const buf) {
-  
+__int64 CEntityKeyValues::Unserialize(CUtlBuffer* const buf) {
+
 }
 
 
