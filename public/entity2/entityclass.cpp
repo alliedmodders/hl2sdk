08823fb6fd4d8032be2c79edd67f7079038b6197
karl-police <karl-police2001@bluewin.ch>
2023-10-01 16:01:16 +0200

    Create a start for entitykeyvalues.cpp

---
+++ public/entity2/entityclass.cpp
@@ -0,0 +1,15 @@
+#include "entitysystem.h"
+
+// idk
+typedef long __int64;
+
+// If it's really __int64 for the function is the other question
+__int64 CEntityClass::Unserialize(/*__int64 a1, __int64 a2, _QWORD *a3*/) {
+  __int64 entity_index; // v8, e.g. ent_attachments entity_index
+  const char* entity_className; // v7, e.g. weapon_glock
+
+  // this gets turned into a string if it's trying to get printed out
+  __int64 entity_classInstance; // v9, e.g. CWeaponGlock
+  
+
+}
