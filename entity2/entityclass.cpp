e1540f6c183b35c331eb2b39781d225a733e7724
karl-police <karl-police2001@bluewin.ch>
2023-10-01 17:37:59 +0200

    Update entityclass.cpp

--- entity2/entityclass.cpp
+++ entity2/entityclass.cpp
@@ -4,18 +4,51 @@
 typedef long __int64;
 
 
+__int64 CEntityClass::UnserializeKey(
+    const CEntityClass::UnserializationContext_t* const context,
+    CEntityInstance* pEntity,
+    char* pFieldMemory,
+    const ComponentUnserializerFieldInfo_t* pFieldInfo,
+    CEntityVariant* value,
+    /*__m128d a7*/)
+{
+
+
+}
+
+
+
+
+
 // still need to figure out what this returns
-__int64 CEntityClass::Unserialize(CEntityClass *const this, CEntityInstance *pEntity, const CEntityKeyValues *pKeyValues) {
+__int64 CEntityClass::Unserialize(CEntityInstance* pEntity, const CEntityKeyValues* pKeyValues) {
 
-    EntityIOConnectionDesc_t *m_Size;
-    void **p_outputs;
+    EntityIOConnectionDesc_t* m_Size;
+    void** p_outputs;
 
-    UnserializeComponentInfo_t *v8; // keep an eye on this thing
-    EntComponentInfo_t *m_pInfo;
+    UnserializeComponentInfo_t* v8; // keep an eye on this thing
+    EntComponentInfo_t* m_pInfo;
 
-    const char *m_pAsString; // v7, e.g. weapon_glock
-    const char *m_pszCPPClassname; // v9, e.g. CWeaponGlock
-  
+    // Some stuff is only used for logging
+    const char* m_pAsString; // v7, e.g. weapon_glock
+    const char* m_pszCPPClassname; // v9, e.g. CWeaponGlock
 
     __int64 entity_index; // v8, e.g. ent_attachments entity_index
+
+
+
+    CEntityClass* c;
+    CEntityClass::UnserializationContext_t context;
+
+    CUtlVector<CEntityIOOutput*, CUtlMemory<CEntityIOOutput*, int> > outputs;
+    EntityIOConnectionDesc_t desc;
+    CUtlVectorFixedGrowable<UnserializeComponentInfo_t, 64ul> components;
+
+
+    c = this;
+
+
+
+
+
 }
