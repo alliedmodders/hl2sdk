5b56691c6b98595bb921e049446dcadae2ffe787
karl-police <karl-police2001@bluewin.ch>
2023-10-01 22:38:06 +0200

    Update entitykeyvalues.cpp

--- entity2/entitykeyvalues.cpp
+++ entity2/entitykeyvalues.cpp
@@ -1,5 +1,36 @@
 #include "entity2/entitysystem.h"
 
+
+struct CEntityKeyValues {
+    CUtlLeanVectorFixedGrowable<unsigned int, 9u, short int> m_keyHashes;
+    CUtlLeanVector<CEntityKeyValues::KeyValueInfo_t, short int> m_keyValues;
+    CUtlScratchMemoryPool m_memoryPool;
+    CEntityKeyValues::EntityComplexKeyListElem_t* m_pComplexKeys;
+    int16 m_nRefCount;
+    int16 m_nQueuedForSpawnCount;
+    CUtlVector<EntityIOConnectionDescFat_t, CUtlMemory<EntityIOConnectionDescFat_t, int> > m_connectionDescs;
+}
+
+
+struct CEntityKeyValues::CIterator {
+    const CEntityKeyValues* m_pKeyValues;
+    int m_nIndex;
+};
+
+struct CEntityKeyValues::EntityComplexKeyListElem_t {
+    IEntityKeyComplex* m_pKey;
+    CEntityKeyValues::EntityComplexKeyListElem_t* m_pNext;
+}
+
+struct CEntityKeyValues::KeyValueInfo_t {
+    CEntityVariant m_value;
+    const char* m_pAttributeName;
+};
+
+
+
+// Functions
+
 void CEntityKeyValues::ReleaseAllComplexKeys()
 {    
     for ( EntityComplexKeyListElem_t* i = m_pComplexKeys; i; i = i->m_pNext )
@@ -21,8 +52,15 @@ void CEntityKeyValues::ReleaseAllComplexKeys()
 
 
 void CEntityKeyValues::RemoveAllKeys() {
-    
+
 }
+
+
+
+bool CEntityKeyValues::HasValue(const CEntityKeyValues* const this, EntityKeyId_t key) {
+
+}
+
 
 // CEntityKeyValues::Serialize
 
@@ -32,6 +70,6 @@ __int64 CEntityKeyValues::Unserialize(CUtlBuffer* const buf) {
 }
 
 
-// CEntityKeyValues::UnserializeKeys from world renderer
+// CEntityKeyValues::UnserializeKeys
 
 // more
