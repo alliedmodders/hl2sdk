1ea8bcb1fc0df2a716ea62f6dc8725bda76bf607
karl-police <karl-police2001@bluewin.ch>
2023-10-01 17:07:47 +0200

    Update entityclass.cpp

--- entity2/entityclass.cpp
+++ entity2/entityclass.cpp
@@ -7,10 +7,8 @@ typedef long __int64;
 // still need to figure out wha this returns
 __int64 CEntityClass::Unserialize(CEntityClass *const this, CEntityInstance *pEntity, const CEntityKeyValues *pKeyValues) {
   __int64 entity_index; // v8, e.g. ent_attachments entity_index
-  const char* entity_className; // v7, e.g. weapon_glock
-
-  // this gets turned into a string if it's trying to get printed out
-  __int64 entity_classInstance; // v9, e.g. CWeaponGlock
+  const char *m_pAsString; // v7, e.g. weapon_glock
+  const char *m_pszCPPClassname; // v9, e.g. CWeaponGlock
   
 
 }
