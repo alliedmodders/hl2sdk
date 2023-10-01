42dae721308f6c7873b00672a118c72d0124ca9a
karl-police <karl-police2001@bluewin.ch>
2023-10-01 17:13:23 +0200

    Update entityclass.cpp

--- entity2/entityclass.cpp
+++ entity2/entityclass.cpp
@@ -1,14 +1,21 @@
 #include "entitysystem.h"
 
-// idk
+// In IDA a __int64 can even be a class
 typedef long __int64;
 
-// In IDA a __int64 can even be a class
-// still need to figure out wha this returns
+
+// still need to figure out what this returns
 __int64 CEntityClass::Unserialize(CEntityClass *const this, CEntityInstance *pEntity, const CEntityKeyValues *pKeyValues) {
-  __int64 entity_index; // v8, e.g. ent_attachments entity_index
-  const char *m_pAsString; // v7, e.g. weapon_glock
-  const char *m_pszCPPClassname; // v9, e.g. CWeaponGlock
+
+    EntityIOConnectionDesc_t *m_Size;
+    void **p_outputs;
+
+    UnserializeComponentInfo_t *v8; // keep an eye on this thing
+    EntComponentInfo_t *m_pInfo;
+
+    const char *m_pAsString; // v7, e.g. weapon_glock
+    const char *m_pszCPPClassname; // v9, e.g. CWeaponGlock
   
 
+    __int64 entity_index; // v8, e.g. ent_attachments entity_index
 }
