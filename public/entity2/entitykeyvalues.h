db04eccdad7d3069bbea2166997578c87e6d17ae
karl-police <karl-police2001@bluewin.ch>
2023-10-02 16:55:05 +0200

    Split into a header file

---
+++ public/entity2/entitykeyvalues.h
@@ -0,0 +1,27 @@
+#include "entity2/entitysystem.h"
+
+struct CEntityKeyValues {
+    CUtlLeanVectorFixedGrowable<unsigned int, 9u, short int> m_keyHashes;
+    CUtlLeanVector<CEntityKeyValues::KeyValueInfo_t, short int> m_keyValues;
+    CUtlScratchMemoryPool m_memoryPool;
+    CEntityKeyValues::EntityComplexKeyListElem_t* m_pComplexKeys;
+    int16 m_nRefCount;
+    int16 m_nQueuedForSpawnCount;
+    CUtlVector<EntityIOConnectionDescFat_t, CUtlMemory<EntityIOConnectionDescFat_t, int> > m_connectionDescs;
+}
+
+
+struct CEntityKeyValues::CIterator {
+    const CEntityKeyValues* m_pKeyValues;
+    int m_nIndex;
+};
+
+struct CEntityKeyValues::EntityComplexKeyListElem_t {
+    IEntityKeyComplex* m_pKey;
+    CEntityKeyValues::EntityComplexKeyListElem_t* m_pNext;
+}
+
+struct CEntityKeyValues::KeyValueInfo_t {
+    CEntityVariant m_value;
+    const char* m_pAttributeName;
+};
