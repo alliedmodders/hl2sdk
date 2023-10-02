f5dce7a43d554267638056366ad1dcfb67e3b73d
karl-police <karl-police2001@bluewin.ch>
2023-10-02 16:55:20 +0200

    Update entitykeyvalues.cpp

--- entity2/entitykeyvalues.cpp
+++ entity2/entitykeyvalues.cpp
@@ -1,35 +1,4 @@
-#include "entity2/entitysystem.h"
-
-
-struct CEntityKeyValues {
-    CUtlLeanVectorFixedGrowable<unsigned int, 9u, short int> m_keyHashes;
-    CUtlLeanVector<CEntityKeyValues::KeyValueInfo_t, short int> m_keyValues;
-    CUtlScratchMemoryPool m_memoryPool;
-    CEntityKeyValues::EntityComplexKeyListElem_t* m_pComplexKeys;
-    int16 m_nRefCount;
-    int16 m_nQueuedForSpawnCount;
-    CUtlVector<EntityIOConnectionDescFat_t, CUtlMemory<EntityIOConnectionDescFat_t, int> > m_connectionDescs;
-}
-
-
-struct CEntityKeyValues::CIterator {
-    const CEntityKeyValues* m_pKeyValues;
-    int m_nIndex;
-};
-
-struct CEntityKeyValues::EntityComplexKeyListElem_t {
-    IEntityKeyComplex* m_pKey;
-    CEntityKeyValues::EntityComplexKeyListElem_t* m_pNext;
-}
-
-struct CEntityKeyValues::KeyValueInfo_t {
-    CEntityVariant m_value;
-    const char* m_pAttributeName;
-};
-
-
-
-// Functions
+#include "public/entity2/entitykeyvalues.h"
 
 void CEntityKeyValues::CEntityKeyValues() { // Only commented things here are missing - "MISSING_HELP"
     CUtlScratchMemoryPool* p_m_memoryPool;
@@ -84,8 +53,7 @@ void CEntityKeyValues::RemoveAllKeys() {
     uint16 m_flags;
 
     
-    // == WARNING ==
-    // This "between" section is unclear and not fully complete
+    // MISSING_HELP
 
 
 
