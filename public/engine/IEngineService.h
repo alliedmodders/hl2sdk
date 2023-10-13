1cb1b4feec084c1e5d64f268c36ed4637be2fdd3
hzqst <113660872@qq.com>
2023-10-13 20:36:34 +0800

    Split LoopMode stuffs into iloopmode.h

--- public/engine/IEngineService.h
+++ public/engine/IEngineService.h
@@ -12,132 +12,16 @@
 
 #include <appframework/IAppSystem.h>
 #include <inputsystem/InputEnums.h>
+#include <iloopmode.h>
+// eventdispatcher.h need to be added when we get CEventDispatcher ready.
 
-class ISwitchLoopModeStatusNotify;
 class IAddonListChangeNotify;
-class ISceneView;
-class ISource2WorldSession;
-class IPrerequisite;
-
-DECLARE_POINTER_HANDLE(PlatWindow_t);
-DECLARE_POINTER_HANDLE(InputContextHandle_t);
-DECLARE_POINTER_HANDLE(SwapChainHandle_t);
-DECLARE_POINTER_HANDLE(ActiveLoop_t);
-DECLARE_POINTER_HANDLE(HSceneViewRenderTarget);
-
-struct RenderViewport_t
-{
-	int m_nVersion;
-	int m_nTopLeftX;
-	int m_nTopLeftY;
-	int m_nWidth;
-	int m_nHeight;
-	float m_flMinZ;
-	float m_flMaxZ;
-};
-
-enum ClientServerMode_t : int32
-{
-	CLIENTSERVERMODE_NONE = 0,
-	CLIENTSERVERMODE_SERVER,
-	CLIENTSERVERMODE_CLIENT,
-	CLIENTSERVERMODE_LISTENSERVER,
-};
-
-struct RHBackColorBuffer_t
-{
-	ISceneView *m_pView;
-	HSceneViewRenderTarget m_hBackColorBuffer;
-};
-
-enum EventMapRegistrationType_t : int32
-{
-	EVENT_MAP_REGISTER = 0,
-	EVENT_MAP_UNREGISTER,
-};
-
-struct EngineLoopState_t
-{
-	PlatWindow_t m_hWnd;
-	SwapChainHandle_t m_hSwapChain;
-	InputContextHandle_t m_hInputContext;
-	int m_nPlatWindowWidth;
-	int m_nPlatWindowHeight;
-	int m_nRenderWidth;
-	int m_nRenderHeight;
-};
 
 struct EventClientOutput_t
 {
 	EngineLoopState_t m_LoopState;
 	float m_flRenderTime;
 	float m_flRealTime;
-};
-
-enum LoopModeType_t : int32
-{
-	LOOP_MODE_TYPE_INVALID = -1,
-	LOOP_MODE_TYPE_SIMPLE = 0,
-	LOOP_MODE_TYPE_CLIENT,
-	LOOP_MODE_TYPE_SERVER,
-	LOOP_MODE_TYPE_HOST,
-
-	LOOP_MODE_TYPE_COUNT,
-};
-
-enum InputHandlerResult_t : int32
-{
-  	IHR_INPUT_IGNORED = 0,
-  	IHR_INPUT_CONSUMED,
-};
-
-abstract_class ILoopType
-{
-public:
-	virtual void AddEngineService(const char* pszName) = 0;
-};
-
-template <typename T>
-struct CBaseCmdKeyValues : T
-{
-	KeyValues* m_pKeyValues;
-};
-
-
-class GameSessionConfiguration_t;
-
-/*class GameSessionConfiguration_t : CBaseCmdKeyValues<CSVCMsg_GameSessionConfiguration>
-{
-};*/
-
-abstract_class ILoopMode
-{
-public:
-	virtual bool LoopInit( KeyValues *pKeyValues, ILoopModePrerequisiteRegistry *pRegistry ) = 0;
-	virtual void LoopShutdown( void ) = 0;
-	virtual void OnLoopActivate( const EngineLoopState_t &state, /*CEventDispatcher<CEventIDManager_Default> **/void* pEventDispatcher) = 0;
-	virtual void OnLoopDeactivate( const EngineLoopState_t &state, /*CEventDispatcher<CEventIDManager_Default> **/void *pEventDispatcher ) = 0;
-	virtual void RegisterEventMap( /*CEventDispatcher<CEventIDManager_Default> **/void* pEventDispatcher, EventMapRegistrationType_t nRegistrationType) = 0;
-	virtual InputHandlerResult_t HandleInputEvent( const InputEvent_t &event, CSplitScreenSlot nSplitScreenPlayerSlot ) = 0;
-	virtual ISceneView* AddViewsToSceneSystem( const EngineLoopState_t &state, double flRenderTime, double flRealTime,
-		const RenderViewport_t &viewport, const RHBackColorBuffer_t &backColorBuffer ) = 0;
-	virtual ISource2WorldSession* GetWorldSession( void ) = 0;
-	virtual ClientServerMode_t GetClientServerMode( void ) = 0;
-	virtual bool ReceivedServerInfo( const GameSessionConfiguration_t &config, ILoopModePrerequisiteRegistry *pRegistry ) = 0;
-	virtual bool IsInHeadlessMode( void ) = 0;
-	virtual void SetFadeColor( Vector4D vColorNormalized ) = 0;
-	virtual bool IsBackgroundMap( void ) = 0;
-	virtual bool IsLevelTransition( void ) = 0;
-};
-
-abstract_class ILoopModeFactory
-{
-public:
-	virtual bool Init( ILoopType *pLoop ) =0;
-	virtual void Shutdown( void ) =0;
-	virtual ILoopMode *CreateLoopMode( void ) =0;
-	virtual void DestroyLoopMode( ILoopMode *pLoopMode ) =0;
-	virtual LoopModeType_t GetLoopModeType( void ) const =0;
 };
 
 abstract_class IEngineService : public IAppSystem
@@ -154,19 +38,6 @@ public:
 	virtual void		RegisterEventMap( /*CEventDispatcher<CEventIDManager_Default> * */ void *pEventDispatcher, EventMapRegistrationType_t nRegistrationType ) = 0;
 	virtual uint16		GetServiceIndex( void ) = 0;
 	virtual void		SetServiceIndex( uint16 index ) = 0;
-};
-
-abstract_class IPrerequisiteRegistry
-{
-public:
-	virtual void RegisterPrerequisite( IPrerequisite * ) = 0;
-};
-
-abstract_class ILoopModePrerequisiteRegistry : public IPrerequisiteRegistry
-{
-public:
-	virtual void LookupLocalizationToken( const char * ) =0;
-	virtual void UnregisterPrerequisite( IPrerequisite * ) =0;
 };
 
 abstract_class IEngineServiceMgr : public IAppSystem, public ILoopModePrerequisiteRegistry
