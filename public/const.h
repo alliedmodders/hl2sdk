857a62d90c5893eb6e18ae770f1fe08420208dfd
zer0.k <61156310+zer0k-z@users.noreply.github.com>
2024-02-26 04:47:41 +0100

    Update CBaseEntity::m_fFlags defines (#213)

--- public/const.h
+++ public/const.h
@@ -105,12 +105,12 @@
 #define	FL_ONGROUND				(1<<0)	// At rest / on the ground
 #define FL_DUCKING				(1<<1)	// Player flag -- Player is fully crouched
 #define	FL_WATERJUMP			(1<<2)	// player jumping out of water
-#define FL_ONTRAIN				(1<<3) // Player is _controlling_ a train, so movement commands should be ignored on client during prediction.
-#define FL_INRAIN				(1<<4)	// Indicates the entity is standing in rain
+#define FL_NOCLIP				(1<<3) // Forces MOVETYPE_NOCLIP on the entity
+#define FL_FAKECLIENT			(1<<4)	// Fake client, simulated server side; don't send network messages to them
 #define FL_FROZEN				(1<<5) // Player is frozen for 3rd person camera
 #define FL_ATCONTROLS			(1<<6) // Player can't move, but keeps key inputs for controlling another entity
 #define	FL_CLIENT				(1<<7)	// Is a player
-#define FL_FAKECLIENT			(1<<8)	// Fake client, simulated server side; don't send network messages to them
+
 #define	FL_INWATER				(1<<9)	// In water
 
 // NOTE if you move things up, make sure to change this value
