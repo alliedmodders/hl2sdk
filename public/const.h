cb0c04d8b27485035f104879838e93c443118fd6
GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>
2024-02-28 08:33:07 +0300

    Update CBaseEntity::m_fFlags defines
    Co-authored-by: zer0.k <61156310+zer0k-z@users.noreply.github.com>

--- public/const.h
+++ public/const.h
@@ -102,16 +102,16 @@
 
 // CBaseEntity::m_fFlags
 // PLAYER SPECIFIC FLAGS FIRST BECAUSE WE USE ONLY A FEW BITS OF NETWORK PRECISION
-#define	FL_ONGROUND				(1<<0)	// At rest / on the ground
-#define FL_DUCKING				(1<<1)	// Player flag -- Player is fully crouched
-#define	FL_WATERJUMP			(1<<2)	// player jumping out of water
-#define FL_NOCLIP				(1<<3) // Forces MOVETYPE_NOCLIP on the entity
-#define FL_FAKECLIENT			(1<<4)	// Fake client, simulated server side; don't send network messages to them
-#define FL_FROZEN				(1<<5) // Player is frozen for 3rd person camera
-#define FL_ATCONTROLS			(1<<6) // Player can't move, but keeps key inputs for controlling another entity
-#define	FL_CLIENT				(1<<7)	// Is a player
-
-#define	FL_INWATER				(1<<9)	// In water
+#define	FL_ONGROUND					(1<<0)	// At rest / on the ground
+#define FL_DUCKING					(1<<1)	// Player flag -- Player is fully crouched
+#define	FL_WATERJUMP				(1<<2)	// player jumping out of water
+#define FL_NOCLIP					(1<<3)	// Forces MOVETYPE_NOCLIP on the entity
+#define FL_PAWN_FAKECLIENT			(1<<4)	// Fake client controlled pawn entity, mostly referenced in movement related code
+#define FL_FROZEN					(1<<5)	// Player is frozen for 3rd person camera
+#define FL_ATCONTROLS				(1<<6)	// Player can't move, but keeps key inputs for controlling another entity
+#define	FL_CLIENT					(1<<7)	// Is a player
+#define FL_CONTROLLER_FAKECLIENT	(1<<8)	// Fake client, simulated server side. Mainly set on controllers
+#define	FL_INWATER					(1<<9)	// In water. Potentially obsolete in s2
 
 // NOTE if you move things up, make sure to change this value
 #define PLAYER_FLAG_BITS		10
@@ -119,21 +119,21 @@
 // NON-PLAYER SPECIFIC (i.e., not used by GameMovement or the client .dll ) -- Can still be applied to players, though
 #define	FL_FLY					(1<<10)	// Changes the SV_Movestep() behavior to not need to be on ground
 #define	FL_SWIM					(1<<11)	// Changes the SV_Movestep() behavior to not need to be on ground (but stay in water)
-#define	FL_CONVEYOR				(1<<12)
-#define	FL_NPC					(1<<13)
+#define	FL_CONVEYOR				(1<<12)	// Potentially obsolete in s2
+#define	FL_NPC					(1<<13) // Potentially obsolete in s2
 #define	FL_GODMODE				(1<<14)
 #define	FL_NOTARGET				(1<<15)
 #define	FL_AIMTARGET			(1<<16)	// set if the crosshair needs to aim onto the entity
-#define	FL_PARTIALGROUND		(1<<17)	// not all corners are valid
+#define	FL_PARTIALGROUND		(1<<17)	// not all corners are valid. Potentially obsolete in s2
 #define FL_STATICPROP			(1<<18)	// Eetsa static prop!		
-#define FL_GRAPHED				(1<<19) // worldgraph has this ent listed as something that blocks a connection
+#define FL_GRAPHED				(1<<19) // worldgraph has this ent listed as something that blocks a connection. Potentially obsolete in s2
 #define FL_GRENADE				(1<<20)
-#define FL_STEPMOVEMENT			(1<<21)	// Changes the SV_Movestep() behavior to not do any processing
+#define FL_STEPMOVEMENT			(1<<21)	// Changes the SV_Movestep() behavior to not do any processing. Potentially obsolete in s2
 #define FL_DONTTOUCH			(1<<22)	// Doesn't generate touch functions, generates Untouch() for anything it was touching when this flag was set
 #define FL_BASEVELOCITY			(1<<23)	// Base velocity has been applied this frame (used to convert base velocity into momentum)
-#define FL_WORLDBRUSH			(1<<24)	// Not moveable/removeable brush entity (really part of the world, but represented as an entity for transparency or something)
+#define FL_CONVEYOR_NEW			(1<<24)
 #define FL_OBJECT				(1<<25) // Terrible name. This is an object that NPCs should see. Missiles, for example.
-#define FL_KILLME				(1<<26)	// This entity is marked for death -- will be freed by game DLL
+#define FL_KILLME				(1<<26)	// This entity is marked for death -- will be freed by game DLL. Potentially obsolete in s2
 #define FL_ONFIRE				(1<<27)	// You know...
 #define FL_DISSOLVING			(1<<28) // We're dissolving!
 #define FL_TRANSRAGDOLL			(1<<29) // In the process of turning into a client side ragdoll.
