4fa74bb79746f4bab315f6856fff8075feae467f
vanz696 <7331.mathew.7331@gmail.com>
2024-03-07 03:07:51 +0300

    Switch to AUTO_LOCK

--- public/schemasystem/schematypes.h
+++ public/schemasystem/schematypes.h
@@ -26,9 +26,9 @@ enum SchemaClassFlags1_t
 	SCHEMA_CF1_HAS_TRIVIAL_CONSTRUCTOR = (1 << 2),
 	SCHEMA_CF1_HAS_TRIVIAL_DESTRUCTOR = (1 << 3),
 	SCHEMA_CF1_LIMITED_METADATA = (1 << 4),
-	SCHEMA_CF1_IS_REGISTERED = (1 << 5),
+	SCHEMA_CF1_INHERITANCE_DEPTH_CALCULATED = (1 << 5),
 	SCHEMA_CF1_MODULE_LOCAL_TYPE_SCOPE = (1 << 6),
-    SCHEMA_CF1_GLOBAL_TYPE_SCOPE = (1 << 7),
+	SCHEMA_CF1_GLOBAL_TYPE_SCOPE = (1 << 7),
 	SCHEMA_CF1_CONSTRUCT_ALLOWED = (1 << 8),
 	SCHEMA_CF1_CONSTRUCT_DISALLOWED = (1 << 9),
 	SCHEMA_CF1_INFO_TAG_MNetworkAssumeNotNetworkable = (1 << 10),
@@ -317,18 +317,18 @@ struct SchemaClassInfoData_t
 	uint16 m_nSingleInheritanceDepth;
 	
 	SchemaClassFieldData_t* m_pFields;
-    SchemaStaticFieldData_t* m_pStaticFields;
-    SchemaBaseClassInfoData_t* m_pBaseClasses;
-    datamap_t* m_pDataDescMap;
-    SchemaMetadataEntryData_t* m_pStaticMetadata;
+	SchemaStaticFieldData_t* m_pStaticFields;
+	SchemaBaseClassInfoData_t* m_pBaseClasses;
+	datamap_t* m_pDataDescMap;
+	SchemaMetadataEntryData_t* m_pStaticMetadata;
 	
-    CSchemaSystemTypeScope* m_pTypeScope;
-    CSchemaType_DeclaredClass* m_pDeclaredClass;
+	CSchemaSystemTypeScope* m_pTypeScope;
+	CSchemaType_DeclaredClass* m_pDeclaredClass;
 	
-    uint m_nFlags1;
-    uint m_nFlags2;
+	uint m_nFlags1;
+	uint m_nFlags2;
 	
-    SchemaClassHandlerFn_t m_pfnHandler;
+	SchemaClassHandlerFn_t m_pfnHandler;
 };
 
 class CSchemaClassInfo : public SchemaClassInfoData_t
