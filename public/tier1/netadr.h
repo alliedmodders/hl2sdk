3162069064c187f880ca8a0feb382adeeaeb971c
GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>
2024-03-18 19:13:05 +0300

    Move netadr to tier0

--- public/tier1/netadr.h
+++ public/tier1/netadr.h
@@ -1,4 +1,4 @@
-//========= Copyright © 1996-2005, Valve Corporation, All rights reserved. ============//
+//========= Copyright ï¿½ 1996-2005, Valve Corporation, All rights reserved. ============//
 //
 // Purpose: 
 //
@@ -13,6 +13,7 @@
 #endif
 
 #include "tier0/platform.h"
+#include "bitbuf.h"
 #undef SetPort
 
 typedef enum
@@ -29,39 +30,57 @@ public:
 	netadr_s() { SetIP( 0 ); SetPort( 0 ); SetType( NA_IP ); }
 	netadr_s( uint unIP, uint16 usPort ) { SetIP( unIP ); SetPort( usPort ); SetType( NA_IP ); }
 	netadr_s( const char *pch ) { SetFromString( pch ); }
-	void	Clear();	// invalids Address
 
-	void	SetType( netadrtype_t type );
-	void	SetPort( unsigned short port );
-	bool	SetFromSockadr(const struct sockaddr *s);
-	void	SetIP(uint8 b1, uint8 b2, uint8 b3, uint8 b4);
-	void	SetIP(uint unIP);									// Sets IP.  unIP is in host order (little-endian)
-	void    SetIPAndPort( uint unIP, unsigned short usPort ) { SetIP( unIP ); SetPort( usPort ); }
-	void	SetFromString(const char *pch, bool bUseDNS = false ); // if bUseDNS is true then do a DNS lookup if needed
-	
-	bool	CompareAdr (const netadr_s &a, bool onlyBase = false) const;
-	bool	CompareClassBAdr (const netadr_s &a) const;
-	bool	CompareClassCAdr (const netadr_s &a) const;
+	// invalids Address
+	DLL_CLASS_IMPORT void	Clear();
 
-	netadrtype_t	GetType() const;
-	unsigned short	GetPort() const;
-	const char*		ToString( bool onlyBase = false ) const; // returns xxx.xxx.xxx.xxx:ppppp
-	void			ToSockadr(struct sockaddr *s) const;
-	unsigned int	GetIP() const;
+	DLL_CLASS_IMPORT void	SetType( netadrtype_t type );
+	DLL_CLASS_IMPORT void	SetPort( unsigned short port );
+	DLL_CLASS_IMPORT bool	SetFromSockadr( const struct sockaddr *s );
 
-	bool	IsLocalhost() const; // true, if this is the localhost IP 
-	bool	IsLoopback() const;	// true if engine loopback buffers are used
-	bool	IsReservedAdr() const; // true, if this is a private LAN IP
-	bool	IsValid() const;	// ip & port != 0
-	bool	IsBaseAdrValid() const;	// ip != 0
+	DLL_CLASS_IMPORT void	SetIP( uint8 b1, uint8 b2, uint8 b3, uint8 b4 );
+	// unIP is in host order (little-endian)
+	DLL_CLASS_IMPORT void	SetIP( uint unIP );
+	DLL_CLASS_IMPORT void   SetIPAndPort( uint unIP, unsigned short usPort ) { SetIP( unIP ); SetPort( usPort ); }
 
-	void    SetFromSocket( int hSocket );
+	// if bUseDNS is true then do a DNS lookup if needed
+	DLL_CLASS_IMPORT void	SetFromString( const char *pch, bool bUseDNS = false );
 
-	// These function names are decorated because the Xbox360 defines macros for ntohl and htonl
- 	unsigned long addr_ntohl() const;
- 	unsigned long addr_htonl() const;
-	bool operator==(const netadr_s &netadr) const {return ( CompareAdr( netadr ) );}
-	bool operator<(const netadr_s &netadr) const;
+	DLL_CLASS_IMPORT bool	CompareAdr( const netadr_s &a, bool onlyBase = false ) const;
+	DLL_CLASS_IMPORT bool	CompareClassBAdr( const netadr_s &a ) const;
+	DLL_CLASS_IMPORT bool	CompareClassCAdr( const netadr_s &a ) const;
+
+	DLL_CLASS_IMPORT netadrtype_t	GetType() const;
+	DLL_CLASS_IMPORT unsigned short	GetPort() const;
+
+	// returns xxx.xxx.xxx.xxx:ppppp
+	DLL_CLASS_IMPORT const char *ToString( bool onlyBase = false ) const;
+	DLL_CLASS_IMPORT void ToString( char *pchBuffer, uint32 unBufferSize, bool onlyBase = false ) const;
+
+	DLL_CLASS_IMPORT void			ToSockadr( struct sockaddr *s ) const;
+
+	DLL_CLASS_IMPORT unsigned int	GetIPHostByteOrder() const;
+	DLL_CLASS_IMPORT unsigned int	GetIPNetworkByteOrder() const;
+	inline unsigned int	GetIP() const { return GetIPNetworkByteOrder(); }
+
+	// true, if this is the localhost IP 
+	DLL_CLASS_IMPORT bool	IsLocalhost() const;
+	// true if engine loopback buffers are used
+	DLL_CLASS_IMPORT bool	IsLoopback() const;
+	// true, if this is a private LAN IP
+	DLL_CLASS_IMPORT bool	IsReservedAdr() const;
+	// ip & port != 0
+	DLL_CLASS_IMPORT bool	IsValid() const;
+	// ip != 0
+	DLL_CLASS_IMPORT bool	IsBaseAdrValid() const;
+
+	DLL_CLASS_IMPORT void    SetFromSocket( int hSocket );
+
+	DLL_CLASS_IMPORT bool    Serialize( bf_write &writeBuf );
+	DLL_CLASS_IMPORT bool    Unserialize( bf_read &readBuf );
+
+	bool operator==( const netadr_s &netadr ) const { return (CompareAdr( netadr )); }
+	DLL_CLASS_IMPORT bool operator<( const netadr_s &netadr ) const;
 
 public:	// members are public to avoid to much changes
 
