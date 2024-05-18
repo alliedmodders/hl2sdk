b851423bd0d039cbc755ce8e87881a64ad2af94f
James Puleo <james@jame.xyz>
2024-05-17 23:14:25 -0400

    Enable `tier0/threadtools.h` to compile under GCC with `-std=c++20`

--- public/tier0/threadtools.h
+++ public/tier0/threadtools.h
@@ -784,7 +784,7 @@ private:
 	MUTEX_TYPE &m_lock;
 
 	// Disallow copying
-	CAutoLockT<MUTEX_TYPE>( const CAutoLockT<MUTEX_TYPE> & );
+	CAutoLockT( const CAutoLockT<MUTEX_TYPE> & );
 	CAutoLockT<MUTEX_TYPE> &operator=( const CAutoLockT<MUTEX_TYPE> & );
 };
 
