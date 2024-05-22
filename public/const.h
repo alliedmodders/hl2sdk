799879cfae2ba0060840a3dfdda62c020190c299
1mpulse <34376386+1mpulse@users.noreply.github.com>
2024-05-22 15:45:57 +0000

    fix v2

--- public/const.h
+++ public/const.h
@@ -146,14 +146,13 @@ enum MoveType_t : unsigned char
 	MOVETYPE_NONE		= 0,	// never moves
 	MOVETYPE_OBSOLETE,			// Previously isometric movetype
 	MOVETYPE_WALK,				// Player only - moving on the ground
-	MOVETYPE_STEP,				// gravity, special edge handling -- monsters use this
 	MOVETYPE_FLY,				// No gravity, but still collides with stuff
 	MOVETYPE_FLYGRAVITY,		// flies through the air + is affected by gravity
 	MOVETYPE_VPHYSICS,			// uses VPHYSICS for simulation
 	MOVETYPE_PUSH,				// no clip to world, push and crush
 	MOVETYPE_NOCLIP,			// No gravity, no collisions, still do velocity/avelocity
-	MOVETYPE_LADDER,			// Used by players only when going onto a ladder
 	MOVETYPE_OBSERVER,			// Observer movement, depends on player's observer mode
+	MOVETYPE_LADDER,			// Used by players only when going onto a ladder
 	MOVETYPE_CUSTOM,			// Allows the entity to describe its own physics
 
 	// should always be defined as the last item in the list
