2176857ea1c981282774493451b49542e160de06
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-10-03 05:31:42 +0300

    Update `ICommandLine` interface

--- public/tier0/icommandline.h
+++ public/tier0/icommandline.h
@@ -11,6 +11,7 @@
 #endif
 
 #include "tier0/platform.h"
+#include "tier0/utlstringtoken.h"
 
 class CBufferString;
 
@@ -25,23 +26,23 @@ public:
 	virtual void		CreateCmdLinePrependAppName( const char *commandline ) = 0;
 
 	// Check whether a particular parameter exists
-	virtual	const char	*CheckParm( const char *psz, const char **ppszValue = 0 ) const = 0;
-	virtual bool		HasParm( const char *psz ) const = 0;
+	virtual	const char	*CheckParm( CUtlStringToken param, const char **ppszValue = 0 ) const = 0;
+	virtual bool		HasParm( CUtlStringToken param ) const = 0;
 	
 	// Gets at particular parameters
 	virtual int			ParmCount() const = 0;
-	virtual int			FindParm( const char *psz ) const = 0;	// Returns 0 if not found.
+	virtual int			FindParm( CUtlStringToken param ) const = 0;	// Returns 0 if not found.
 	virtual const char* GetParm( int nIndex ) const = 0;
 
 	// Returns the argument after the one specified, or the default if not found
-	virtual const char	*ParmValue( const char *psz, const char *pDefaultVal = 0 ) const = 0;
-	virtual int			ParmValue( const char *psz, int nDefaultVal ) const = 0;
-	virtual float		ParmValue( const char *psz, float flDefaultVal ) const = 0;
-	virtual bool		ParmValue( const char *psz, const char *pDefaultVal, CBufferString *bufOut ) = 0;
+	virtual const char	*ParmValue( CUtlStringToken param, const char *pDefaultVal = 0 ) const = 0;
+	virtual int			ParmValue( CUtlStringToken param, int nDefaultVal ) const = 0;
+	virtual float		ParmValue( CUtlStringToken param, float flDefaultVal ) const = 0;
+	virtual bool		ParmValue( CUtlStringToken param, const char *pDefaultVal, CBufferString *bufOut ) = 0;
 
 	virtual const char **GetParms() const = 0;
 	virtual const char *GetCmdLine( void ) const = 0;
-	virtual void		AppendParm( const char *pszParm, const char *pszValues ) = 0;
+	virtual void		AppendParm( CUtlStringToken param, const char *pszValues ) = 0;
 	
 	// Returns true if there's atleast one parm available
 	virtual bool		HasParms( void ) const = 0;
