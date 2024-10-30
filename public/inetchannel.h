612899ef03bf682cf83f0d7068e96201843cc56f
hzqst <113660872@qq.com>
2024-10-30 15:06:08 +0800

    revert the formatting back, and also update INetworkMessageProcessingPreFilter::FilterMessage

--- public/inetchannel.h
+++ public/inetchannel.h
@@ -59,96 +59,91 @@ abstract_class INetworkMessageProcessingPreFilter
 {
 public:
 	// Filter incoming messages from the netchan, return true to filter out (block) the further processing of the message
-	virtual bool FilterMessage( INetworkMessageInternal *pNetMessage, const CNetMessage *pData, INetChannel *pChannel ) = 0;
+	virtual bool FilterMessage( const CNetMessage *pData, INetChannel *pChannel ) = 0;
 };
 
 abstract_class INetChannel : public INetChannelInfo
 {
 public:
-	virtual	~INetChannel( void ) {};																	//31
+	virtual	~INetChannel( void ) {};
 
-	virtual void	Reset( void ) = 0;																	//32
-	virtual void	Clear( void ) = 0;																	//33
+	virtual void	Reset( void ) = 0;
+	virtual void	Clear( void ) = 0;
+	virtual void	Shutdown( ENetworkDisconnectionReason reason ) = 0;
 	
-	virtual void	Shutdown( ENetworkDisconnectionReason reason ) = 0;									//34
+	virtual float	sub_180095190_GetUnknownFloat() const = 0;
 	
-	virtual float	sub_180095190_GetUnknownFloat() const = 0;											//35
-	
-	virtual bool	SendNetMessage(void* messageHandle, google::protobuf::Message* message, NetChannelBufType_t type) = 0;//36
-	virtual bool	SendData(bf_write& msg, NetChannelBufType_t bufferType) = 0;						//37
-	
-	virtual int		Transmit(const char* pDebugName, bf_write* data) = 0;								//38
-	virtual void	SetBitsToSend() = 0;																//39
-	virtual int		SendMessages(const char* pDebugName, bf_write* data) = 0;							//40
-	virtual void	ClearBitsToSend() = 0;																//41
+	virtual bool	SendNetMessage( const CNetMessage *pData, NetChannelBufType_t bufType ) = 0;
+	virtual bool	SendData( bf_write &msg, NetChannelBufType_t bufferType ) = 0;
+	virtual int		Transmit( const char *pDebugName, bf_write *data ) = 0;
+	virtual void	SetBitsToSend( void ) = 0;
+	virtual int		SendMessages( const char *pDebugName, bf_write *data ) = 0;
+	virtual void	ClearBitsToSend( void ) = 0;
 
-	virtual const netadr_t& GetRemoteAddress() = 0;														//42
+	virtual const netadr_t &GetRemoteAddress( void ) const = 0;
+
+	virtual void	UpdateMessageStats( int msggroup, int bits, bool ) = 0;
 	
-	virtual void	UpdateMessageStats(int msggroup, int bits, bool) = 0;								//43
+	virtual void	unk001( void ) = 0;
 	
-	virtual void	sub_18009DA80_Unknown(void) = 0;													//44
-	
-	virtual bool	CanPacket(void) const = 0;															//45
-	virtual bool	IsOverflowed(void) const = 0;														//46
-	virtual bool	HasPendingReliableData(void) = 0;													//47
-	
+	virtual bool	CanPacket( void ) const = 0;
+	virtual bool	IsOverflowed( void ) const = 0;
+	virtual bool	HasPendingReliableData( void ) = 0;
+
 	// For routing messages to a different handler
-	virtual void	SetActiveChannel(INetChannel * pNewChannel) = 0;									//48
-
-	virtual void	AttachSplitPlayer(int CSplitScreenSlot, INetChannel*) = 0;							//49
-	virtual void	DetachSplitPlayer(int CSplitScreenSlot) = 0;										//50
-
-	virtual void	Setup(void) = 0;																	//51
-	virtual void	Setup2(void) = 0;																	//52
-
-	virtual void	SetTimeout(float seconds, bool bForceExact = false) = 0;							//53
-	virtual bool	IsTimedOut(void) const = 0;															//54
-	virtual void	UpdateLastReceivedTime(void) = 0;													//55
-
-	virtual float	sub_180096C00_GetUnknownFloat() const = 0;											//56
-	//idk, can this be SetRemoteFramerate?
-	virtual void	sub_180097DA0_SetUnknown(void *pUnknownStruct) = 0;									//57
-
-	virtual bool	IsRemoteDisconnected(ENetworkDisconnectionReason& reason) const = 0;				//58
-
-	virtual void	SetNetMessageDispatcher(INetMessageDispatcher* pDispatcher) = 0;					//59
-	virtual INetMessageDispatcher* GetNetMessageDispatcher(void) const = 0;								//60
-
-	virtual void	StartRegisteringMessageHandlers( void ) = 0;										//61
-	virtual void	FinishRegisteringMessageHandlers( void ) = 0;										//62
-
-	virtual void	RegisterNetMessageHandlerAbstract( CUtlSlot *nSlot, const CUtlAbstractDelegate &delegate, int nParamCount, INetworkMessageInternal *pNetMessage, int nPriority ) = 0;//63
-	virtual void	UnregisterNetMessageHandlerAbstract( CUtlSlot *nSlot, const CUtlAbstractDelegate &delegate, INetworkMessageInternal *pNetMessage ) = 0;//64
+	virtual bool	SetActiveChannel( INetChannel *pNewChannel ) = 0;
 	
-	virtual int		GetNumBitsWritten( NetChannelBufType_t bufferType ) const = 0;						//65
+	virtual void	AttachSplitPlayer( CSplitScreenSlot nSlot, INetChannel *pChannel ) = 0;
+	virtual void	DetachSplitPlayer( CSplitScreenSlot nSlot ) = 0;
 
-	virtual void	SetDemoRecorder( IDemoRecorderBase *pDemoRecorder ) = 0;							//66
-	virtual void	SetInstantReplayIntercept( IInstantReplayIntercept *pInstantReplayIntercept ) = 0;	//67
-	
-	virtual bool	IsNull( void ) const = 0;															//68
-	
-	virtual bool	ProcessDemoPacket( NetPacket_t *packet ) = 0;										//69
+	virtual void	SetMinDataRate( int rate ) = 0;
+	virtual void	SetMaxDataRate( int rate ) = 0;
 
-	virtual void	InstallMessageFilter( INetworkMessageProcessingPreFilter *pFilter ) = 0;			//70
-	virtual void	UninstallMessageFilter( INetworkMessageProcessingPreFilter *pFilter ) = 0;			//71
+	virtual void	SetTimeout( float seconds, bool bForceExact = false ) = 0;
+	virtual bool	IsTimedOut( void ) const = 0;
+	virtual void	UpdateLastReceivedTime( void ) = 0;
+
+	virtual float	sub_180096C00_GetUnknownFloat() const = 0;
+	//Can this be SetRemoteFramerate?
+	virtual void	sub_180097DA0_SetUnknown(void *pUnknownStruct) = 0;
+	virtual bool	IsRemoteDisconnected( ENetworkDisconnectionReason &reason ) const = 0;
+
+	virtual void	SetNetMessageDispatcher( INetMessageDispatcher *pDispatcher ) = 0;
+	virtual INetMessageDispatcher *GetNetMessageDispatcher( void ) const = 0;
 	
-	virtual void	PostReceivedNetMessage( INetworkMessageInternal *pNetMessage, const CNetMessage *pData, const NetChannelBufType_t *pBufType, int nBits, int nInSequenceNr ) = 0;//72
+	virtual void	StartRegisteringMessageHandlers( void ) = 0;
+	virtual void	FinishRegisteringMessageHandlers( void ) = 0;
 	
-	virtual void	InsertReplayMessage( InstantReplayMessage_t &msg ) = 0;								//73
+	virtual void	RegisterNetMessageHandlerAbstract( CUtlSlot *nSlot, const CUtlAbstractDelegate &delegate, int nParamCount, INetworkMessageInternal *pNetMessage, int nPriority ) = 0;
+	virtual void	UnregisterNetMessageHandlerAbstract( CUtlSlot *nSlot, const CUtlAbstractDelegate &delegate, INetworkMessageInternal *pNetMessage ) = 0;
 	
-	virtual bool	HasQueuedNetMessages( unsigned short nMessageId ) const = 0;						//74
+	virtual int		GetNumBitsWritten( NetChannelBufType_t bufferType ) const = 0;
+	virtual void	SetDemoRecorder( IDemoRecorderBase *pDemoRecorder ) = 0;
+	virtual void	SetInstantReplayIntercept( IInstantReplayIntercept *pInstantReplayIntercept ) = 0;
+	virtual bool	IsNull( void ) const = 0;
+	virtual bool	ProcessDemoPacket( NetPacket_t *packet ) = 0;
 	
-	virtual void	SetPendingDisconnect( ENetworkDisconnectionReason reason ) = 0;						//75
-	virtual ENetworkDisconnectionReason GetPendingDisconnect( void ) const = 0;							//76
+	virtual void	InstallMessageFilter( INetworkMessageProcessingPreFilter *pFilter ) = 0;
+	virtual void	UninstallMessageFilter( INetworkMessageProcessingPreFilter *pFilter ) = 0;
 	
-	virtual void	SuppressTransmit( bool suppress ) = 0;												//77
-	virtual bool	IsSuppressingTransmit( void ) const = 0;											//78
+	virtual void	PostReceivedNetMessage( INetworkMessageInternal *pNetMessage, const CNetMessage *pData, const NetChannelBufType_t *pBufType, int nBits, int nInSequenceNr ) = 0;
+	virtual void	InsertReplayMessage( InstantReplayMessage_t &msg ) = 0;
+	virtual bool	HasQueuedNetMessages( int nMessageId ) const = 0;
+
+	virtual void	SetPendingDisconnect( ENetworkDisconnectionReason reason ) = 0;
+	virtual ENetworkDisconnectionReason GetPendingDisconnect( void ) const = 0;
+
+	virtual void	SuppressTransmit( bool suppress ) = 0;
+	virtual bool	IsSuppressingTransmit( void ) const = 0;
 	
-	virtual EResult	SendRawMessage( const void *pData, uint32 cbData, int nSendFlags ) = 0;				//79
+	virtual EResult	SendRawMessage( const void *pData, uint32 cbData, int nSendFlags ) = 0;
 	
-	virtual void	sub_18009CB70_SetUnknown(int a1) = 0;												//80
-	virtual void	sub_18009DAE0_GetUnknown() = 0;														//81
-	virtual void	sub_180097E50_SetUnknown(void *pUnknownStruct) = 0;									//82
+	virtual int		GetCurrentNetMessageBits( void ) const = 0;
+	virtual int		GetCurrentNetMessageInSequenceNr( void ) const = 0;
+
+	virtual void	sub_18009CB70_SetUnknown(int a1) = 0;
+	virtual void	sub_18009DAE0_GetUnknown() = 0;
+	virtual void	sub_180097E50_SetUnknown(void *pUnknownStruct) = 0;
 };
 
 
--- public/inetchannelinfo.h
+++ public/inetchannelinfo.h
@@ -53,46 +53,45 @@ public:
 		TOTAL,			// must be last and is not a real group
 	};
 	
-
-	virtual const char  *GetName( void ) const = 0;			// get channel name						// 0
-	virtual const char  *GetAddress( void ) const = 0;		// get channel IP address as string		// 1
-	virtual float		GetTime( void ) const = 0;			// current net time						// 2
-	virtual float		GetTimeConnected( void ) const = 0;	// get connection time in seconds		// 3
-	virtual int			GetBufferSize( void ) const = 0;	// netchannel packet history size		// 4
-	virtual int			GetDataRate( void ) const = 0;		// send data rate in byte/sec			// 5
-
-	virtual bool		IsLocalHost( void ) const = 0;	// true if localhost						// 6	
-	virtual bool		IsLoopback( void ) const = 0;	// true if loopback channel					// 7
-	virtual bool		IsTimingOut( void ) const = 0;	// true if timing out						// 8
-	virtual bool		IsPlayback( void ) const = 0;	//always true								// 9
-
-	virtual float		GetAvgLatency( void ) const = 0;	// average packet latency in seconds					// 10
-	virtual float		GetEngineLatency( void ) const = 0;	// current latency (RTT), more accurate but jittering	// 11
-
-	virtual float		GetAvgLoss( int flow ) const = 0;	 // avg packet loss[0..1]				// 12
-	virtual float		GetAvgChoke(int flow) const = 0;	 //										// 13
-	virtual float		GetAvgData( int flow ) const = 0;	 // data flow in bytes/sec				// 14
-	virtual float		GetAvgPackets( int flow ) const = 0; // avg packets/sec						// 15
-	virtual int			GetTotalData( int flow ) const = 0;	 // total flow in/out in bytes			// 16
-	virtual int			GetTotalPackets( int flow ) const = 0;										// 17
-	virtual int			GetSequenceNr( int flow ) const = 0;	// last send seq number				// 18
-	virtual float		GetTimeSinceLastReceived( void ) const = 0;	// get time since last recieved packet in seconds	//19
-	virtual void		GetRemoteFramerate( float *a1, float *a2, float *a3 ) const = 0;			//20
+	virtual const char  *GetName( void ) const = 0;	// get channel name
+	virtual const char  *GetAddress( void ) const = 0; // get channel IP address as string
+	virtual float		GetTime( void ) const = 0;	// current net time
+	virtual float		GetTimeConnected( void ) const = 0;	// get connection time in seconds
+	virtual int			GetBufferSize( void ) const = 0;	// netchannel packet history size
+	virtual int			GetDataRate( void ) const = 0; // send data rate in byte/sec
 	
-	virtual float		GetTimeoutSeconds( void ) const = 0;										//21
-	virtual float		GetTimeUntilTimeout( void ) const = 0;										//22
+	virtual bool		IsLocalHost( void ) const = 0;	// true if localhost
+	virtual bool		IsLoopback( void ) const = 0;	// true if loopback channel
+	virtual bool		IsTimingOut( void ) const = 0;	// true if timing out
+	virtual bool		IsPlayback( void ) const = 0;
 
-	virtual void		sub_180096C10_GetUnknownFloat() const = 0;									//23
-
-	virtual void		ResetLatencyStats( int channel ) = 0;										//24
-	virtual SNetChannelLatencyStats *GetLatencyStats( int channel ) const = 0;						//25
-	virtual void		SetLatencyStats( int channel, const SNetChannelLatencyStats &stats ) = 0;	//26
-
-	virtual void		SetInterpolationAmount(float flInterpolationAmount, float flUpdateRate) = 0;//27
-	virtual void		SetNumPredictionErrors( int a1 ) = 0;										//28
-	virtual void		SetShowNetMessages(bool bShow) = 0;											//29
+	virtual float		GetAvgLatency( void ) const = 0;	// average packet latency in seconds
+	virtual float		GetEngineLatency( void ) const = 0;	// current latency (RTT), more accurate but jittering
 	
-	virtual int			sub_18009CF60_GetUnknownCount(int a1, unsigned short* a2, int maxCount) = 0;//30
+	virtual float		GetAvgLoss( int flow ) const = 0;	 // avg packet loss[0..1]
+	virtual float		GetAvgChoke(int flow) const = 0;
+	virtual float		GetAvgData( int flow ) const = 0;	 // data flow in bytes/sec
+	virtual float		GetAvgPackets( int flow ) const = 0; // avg packets/sec
+	virtual int			GetTotalData( int flow ) const = 0;	 // total flow in/out in bytes
+	virtual int			GetTotalPackets( int flow ) const = 0;
+	virtual int			GetSequenceNr( int flow ) const = 0;	// last send seq number
+	virtual float		GetTimeSinceLastReceived( void ) const = 0;	// get time since last recieved packet in seconds
+	virtual void		GetRemoteFramerate( float *pflFrameTime, float *pflFrameTimeStdDeviation, float *pflFrameStartTimeStdDeviation ) const = 0;
+
+	virtual float		GetTimeoutSeconds( void ) const = 0;
+	virtual float		GetTimeUntilTimeout( void ) const = 0;
+	
+	virtual void		unk101( void ) const = 0;
+	
+	virtual void		ResetLatencyStats( int channel ) = 0;
+	virtual SNetChannelLatencyStats *GetLatencyStats( int channel ) const = 0;
+	virtual void		SetLatencyStats( int channel, const SNetChannelLatencyStats &stats ) = 0;
+	
+	virtual void		SetInterpolationAmount( float flInterpolationAmount, float flUpdateRate ) = 0;
+	virtual void		SetNumPredictionErrors( int num ) = 0;
+	virtual void		SetShowNetMessages( bool show ) = 0;
+
+	virtual int			sub_18009CF60_GetUnknownCount(int a1, unsigned short* a2, int maxCount) = 0;		//30
 };
 
 #endif // INETCHANNELINFO_H
