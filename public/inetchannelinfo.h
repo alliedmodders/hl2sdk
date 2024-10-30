5f6f9c2de5b18b6a1e384e9a3991b03a5bc90f8e
hzqst <113660872@qq.com>
2024-10-30 14:40:41 +0800

    revert the formatting

--- public/inetchannelinfo.h
+++ public/inetchannelinfo.h
@@ -60,12 +60,15 @@ public:
 	virtual float		GetTimeConnected( void ) const = 0;	// get connection time in seconds		// 3
 	virtual int			GetBufferSize( void ) const = 0;	// netchannel packet history size		// 4
 	virtual int			GetDataRate( void ) const = 0;		// send data rate in byte/sec			// 5
+
 	virtual bool		IsLocalHost( void ) const = 0;	// true if localhost						// 6	
 	virtual bool		IsLoopback( void ) const = 0;	// true if loopback channel					// 7
 	virtual bool		IsTimingOut( void ) const = 0;	// true if timing out						// 8
 	virtual bool		IsPlayback( void ) const = 0;	//always true								// 9
-	virtual float		GetLatency(void) const = 0;	// average packet latency in seconds			// 10
-	virtual float		GetAvgLatency( void ) const = 0;	// average packet latency in seconds	// 11
+
+	virtual float		GetAvgLatency( void ) const = 0;	// average packet latency in seconds					// 10
+	virtual float		GetEngineLatency( void ) const = 0;	// current latency (RTT), more accurate but jittering	// 11
+
 	virtual float		GetAvgLoss( int flow ) const = 0;	 // avg packet loss[0..1]				// 12
 	virtual float		GetAvgChoke(int flow) const = 0;	 //										// 13
 	virtual float		GetAvgData( int flow ) const = 0;	 // data flow in bytes/sec				// 14
@@ -75,16 +78,21 @@ public:
 	virtual int			GetSequenceNr( int flow ) const = 0;	// last send seq number				// 18
 	virtual float		GetTimeSinceLastReceived( void ) const = 0;	// get time since last recieved packet in seconds	//19
 	virtual void		GetRemoteFramerate( float *a1, float *a2, float *a3 ) const = 0;			//20
+	
 	virtual float		GetTimeoutSeconds( void ) const = 0;										//21
 	virtual float		GetTimeUntilTimeout( void ) const = 0;										//22
+
 	virtual void		sub_180096C10_GetUnknownFloat() const = 0;									//23
+
 	virtual void		ResetLatencyStats( int channel ) = 0;										//24
 	virtual SNetChannelLatencyStats *GetLatencyStats( int channel ) const = 0;						//25
 	virtual void		SetLatencyStats( int channel, const SNetChannelLatencyStats &stats ) = 0;	//26
+
 	virtual void		SetInterpolationAmount(float flInterpolationAmount, float flUpdateRate) = 0;//27
 	virtual void		SetNumPredictionErrors( int a1 ) = 0;										//28
 	virtual void		SetShowNetMessages(bool bShow) = 0;											//29
-	virtual int			sub_18009CF60_GetUnknownCount(int a1, unsigned short* a2, int maxCount) = 0;		//30
+	
+	virtual int			sub_18009CF60_GetUnknownCount(int a1, unsigned short* a2, int maxCount) = 0;//30
 };
 
 #endif // INETCHANNELINFO_H
