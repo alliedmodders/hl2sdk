e18a878f914c994c847eb9f0564df49a4d8ffcd0
GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>
2024-11-13 12:09:42 +0300

    Rename to match other functions

--- public/inetchannelinfo.h
+++ public/inetchannelinfo.h
@@ -94,8 +94,8 @@ public:
 	virtual void		SetNumPredictionErrors( int num ) = 0;
 	virtual void		SetShowNetMessages( bool show ) = 0;
 
-	virtual void		unk200() = 0;
 	virtual void		unk201() = 0;
+	virtual void		unk202() = 0;
 };
 
 #endif // INETCHANNELINFO_H
