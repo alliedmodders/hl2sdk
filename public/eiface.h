426ec93bbdb479231e49b1e2eae00dd49f4ad366
GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>
2025-07-31 20:25:54 +0300

    Update IVEngineServer2

--- public/eiface.h
+++ public/eiface.h
@@ -182,6 +182,8 @@ public:
 	virtual const char *GetFrameTimeAmnesty( bool check_cvar ) = 0;
 	virtual void		ShowFrameTimeReport( void *, bool ) = 0;
 
+	virtual void		DumpNetStats( void *, void * ) = 0;
+
 	// Tell engine to change level ( "changelevel s1\n" or "changelevel2 s1 s2\n" )
 	virtual void		ChangeLevel( const char *s1, const char *s2 ) = 0;
 
