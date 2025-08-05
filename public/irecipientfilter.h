4cb7945a57a24121347e5cad27f6a550719eaed8
hzqst <113660872@qq.com>
2025-08-05 14:26:22 +0800

    update IRecipientFilter.

--- public/irecipientfilter.h
+++ public/irecipientfilter.h
@@ -25,8 +25,11 @@ public:
 	virtual NetChannelBufType_t	GetNetworkBufType( void ) const = 0;
 	virtual bool	IsInitMessage( void ) const = 0;
 
-	virtual int 	GetRecipientCount( void ) const = 0;
-	virtual CPlayerSlot	GetRecipientIndex( int slot ) const = 0;
+	virtual uint32_t *GetRecipientBitmasks( void ) const = 0; // return &uint32_t[2]
+
+	//Removed since AnimGraph2 update
+	//virtual int 	GetRecipientCount( void ) const = 0;
+	//virtual CPlayerSlot	GetRecipientIndex( int slot ) const = 0;
 };
 
 #endif // IRECIPIENTFILTER_H
