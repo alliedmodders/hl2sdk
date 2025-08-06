4f3cce2955b8bd29f0ae1170e16093370dff2f0a
GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>
2025-08-06 21:38:10 +0300

    Cosmetic changes

--- public/entity2/entityinstance.h
+++ public/entity2/entityinstance.h
@@ -18,11 +18,14 @@ struct CEntityPrecacheContext;
 struct ChangeAccessorFieldPathIndexInfo_t;
 struct datamap_t;
 
-
 struct NetworkStateChangedData
 {
-	inline NetworkStateChangedData() : m_Unk00(1), m_nLine(-1), m_nArrayIndex(-1), m_nPathIndex(ChangeAccessorFieldPathIndex_t()), m_Unk60(0) { }
-	inline explicit NetworkStateChangedData( bool bFullChanged ) : m_Unk00(static_cast<uint32>(!bFullChanged)), m_nLine(-1), m_nArrayIndex(-1), m_nPathIndex(ChangeAccessorFieldPathIndex_t()), m_Unk60(0) { }
+	inline NetworkStateChangedData() : m_unk001(1), m_FieldOffset(-1), m_nArrayIndex(-1), m_nPathIndex(ChangeAccessorFieldPathIndex_t()), m_unk101(0) { }
+	inline explicit NetworkStateChangedData( bool bFullChanged ) :
+		m_unk001(static_cast<uint32>(!bFullChanged)), m_FieldOffset(-1),
+		m_nArrayIndex(-1), m_nPathIndex(ChangeAccessorFieldPathIndex_t()),
+		m_unk101(0)
+	{ }
 
 	// nLocalOffset is the flattened field offset
 	//		calculated taking into account embedded structures
@@ -32,12 +35,14 @@ struct NetworkStateChangedData
 	// nPathIndex is the value to specify 
 	//		if the path to the field goes through one or more pointers, otherwise pass -1
 	// 		this value is usually a member of the CNetworkVarChainer and belongs to the last object in the chain
-	inline NetworkStateChangedData( uint32 nLocalOffset, int32 nArrayIndex = -1, ChangeAccessorFieldPathIndex_t nPathIndex = ChangeAccessorFieldPathIndex_t() ) : m_Unk00(1), m_LocalOffsets(0, 1), m_nLine(-1), m_nArrayIndex(nArrayIndex), m_nPathIndex(nPathIndex), m_Unk60(0)
+	inline NetworkStateChangedData( uint32 nLocalOffset, int32 nArrayIndex = -1, ChangeAccessorFieldPathIndex_t nPathIndex = ChangeAccessorFieldPathIndex_t() ) :
+		m_unk001(1), m_LocalOffsets(0, 1), m_FieldOffset(-1), m_nArrayIndex(nArrayIndex), m_nPathIndex(nPathIndex), m_unk101(0)
 	{
 		m_LocalOffsets.AddToHead(nLocalOffset);
 	}
 
-	inline NetworkStateChangedData(const std::initializer_list< uint32 > nLocalOffsets, int32 nArrayIndex = -1, ChangeAccessorFieldPathIndex_t nPathIndex = ChangeAccessorFieldPathIndex_t()) : m_Unk00(1), m_LocalOffsets(0, nLocalOffsets.size()), m_nLine(-1), m_nArrayIndex(nArrayIndex), m_nPathIndex(nPathIndex), m_Unk60(1)
+	inline NetworkStateChangedData(const std::initializer_list< uint32 > nLocalOffsets, int32 nArrayIndex = -1, ChangeAccessorFieldPathIndex_t nPathIndex = ChangeAccessorFieldPathIndex_t()) :
+		m_unk001(1), m_LocalOffsets(0, nLocalOffsets.size()), m_FieldOffset(-1), m_nArrayIndex(nArrayIndex), m_nPathIndex(nPathIndex), m_unk101(1)
 	{
 		for ( const uint32& nLocalOffset : nLocalOffsets )
 		{
@@ -45,25 +50,18 @@ struct NetworkStateChangedData
 		}
 	}
 
-	uint32 m_Unk00; // Perhaps it is an enum, default 1, when 0 adds FL_FULL_EDICT_CHANGED
+	uint32 m_unk001; // Perhaps it is an enum, default 1, when 0 adds FL_FULL_EDICT_CHANGED
 	CUtlVector<uint32> m_LocalOffsets;
-	// Probably only works in the debug build, as it has always been empty
+	
+	// AMNOTE: Mostly unused/debug
+	CUtlString m_ClassName;
 	CUtlString m_FieldName;
-	CUtlString m_FileName;
-	int32 m_nLine; // default -1
-	int32 m_nArrayIndex; // default -1
-	ChangeAccessorFieldPathIndex_t m_nPathIndex; // default -1 (can also be -2)
-	int16 m_Unk60; // default 0, if m_LocalOffsets has multiple values, it is set to 1
-};
-COMPILE_TIME_ASSERT(sizeof(NetworkStateChangedData) == 64);
+	int32 m_FieldOffset;
+	int32 m_nArrayIndex;
+	ChangeAccessorFieldPathIndex_t m_nPathIndex;
 
-// Not entirely sure
-struct NetworkStateChanged3Data
-{
-	CUtlVector<uint32> m_Unk0;
-	CUtlVector<uint32> m_Unk24;
+	int16 m_unk101; // default 0, if m_LocalOffsets has multiple values, it is set to 1
 };
-
 
 class CEntityInstance
 {
@@ -105,28 +103,33 @@ public:
 	virtual CEntityIndex RequiredEdictIndex() = 0;
 
 	// marks a field for transmission over the network
-	virtual void NetworkStateChanged( const NetworkStateChangedData& data ) = 0; // Function replaces old version NetworkStateChanged( uint nOffset, int, ChangeAccessorFieldPathIndex_t PathIndex )
-	virtual void NetworkStateChangedBranch( const void* data ) = 0; // Game never call this function during testing
-	virtual void NetworkStateChanged_3( const NetworkStateChanged3Data& data ) = 0;
-	virtual void NetworkStateUnkSetBool( bool bUnk ) = 0; // Affects behavior of NetworkStateChanged
+	virtual void NetworkStateChanged( const NetworkStateChangedData& data ) = 0;
+
+	// AMNOTE: NetworkState related methods
+	virtual void unk201( const void* data ) = 0;
+	virtual void unk202( const void* data ) = 0;
+
+	// Toggles network update state, if set to false would skip network updates
+	virtual void NetworkUpdateState( bool state ) = 0;
 	virtual void NetworkStateChangedLog( const char* pszFieldName, const char* pszInfo ) = 0;
+
 	virtual bool FullEdictChanged() = 0;
 
-	virtual void unk201() = 0;
-	virtual void unk202() = 0;
+	virtual void unk301() = 0;
+	virtual void unk302() = 0;
 
 	virtual ChangeAccessorFieldPathIndex_t AddChangeAccessorPath( const CFieldPath& path ) = 0;
 	virtual void AssignChangeAccessorPathIds() = 0;
 	virtual ChangeAccessorFieldPathIndexInfo_t* GetChangeAccessorPathInfo_1() = 0;
 	virtual ChangeAccessorFieldPathIndexInfo_t* GetChangeAccessorPathInfo_2() = 0;
 	
-	virtual void unk301() = 0;
-	virtual void unk302() = 0;
+	virtual void unk401() = 0;
+	virtual void unk402() = 0;
 
 	virtual void ReloadPrivateScripts() = 0;
 	virtual datamap_t* GetDataDescMap() = 0;
 
-	virtual void unk401() = 0;
+	virtual void unk501() = 0;
 
 	virtual SchemaMetaInfoHandle_t<CSchemaClassInfo> Schema_DynamicBinding() = 0;
 
@@ -153,8 +156,10 @@ private:
 	void* m_hPrivateScope; // 0x18 - CEntityPrivateScriptScope
 public:
 	CEntityKeyValues* m_pKeyValues; // 0x20
-	CScriptComponent* m_CScriptComponent; // 0x28
-	bool m_bVisibleinPVS; // 0x30
+private:
+	uint8 pad[8];
+public:
+	CScriptComponent* m_CScriptComponent; // 0x30
 };
 
 // -------------------------------------------------------------------------------------------------- //
