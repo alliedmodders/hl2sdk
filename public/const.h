bc0375a4127626df03be00bd3a54568864a5b95d
Michael Barth <Spirrwell@gmail.com>
2025-08-10 10:23:23 -0400

    Increase edict limit to 8192

--- public/const.h
+++ public/const.h
@@ -62,7 +62,7 @@
 #define SP_MODEL_INDEX_BITS			13
 
 // How many bits to use to encode an edict.
-#define	MAX_EDICT_BITS				11			// # of bits needed to represent max edicts
+#define	MAX_EDICT_BITS				13			// # of bits needed to represent max edicts
 // Max # of edicts in a level
 #define	MAX_EDICTS					(1<<MAX_EDICT_BITS)
 
