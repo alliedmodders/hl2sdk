e84d5814138bbc32d3c716e7d755223a54b53be2
Vauff <mctehkitti@gmail.com>
2025-09-09 16:25:36 -0400

    Fix CNetworkGameServerBase::ConnectClient param type

--- public/iserver.h
+++ public/iserver.h
@@ -188,7 +188,7 @@ public:
 
 	virtual void	StartHLTVMaster() = 0;
 
-	virtual CServerSideClientBase *ConnectClient( const char *pszName, ns_address *pAddr, void *pNetInfo, C2S_CONNECT_Message *pConnectMsg,
+	virtual CServerSideClientBase *ConnectClient( const char *pszName, ns_address *pAddr, uint32 steam_handle, C2S_CONNECT_Message *pConnectMsg,
 												  const char *pszChallenge, const byte *pAuthTicket, int nAuthTicketLength, bool bIsLowViolence ) = 0;
 	virtual CServerSideClientBase *CreateNewClient( CPlayerSlot slot ) = 0;
 	
