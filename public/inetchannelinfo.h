87595b45cfc2531b6ff9d7f3a73a608a18922b24
GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>
2025-10-15 16:00:56 +0300

    Update INetChannelInfo

--- public/inetchannelinfo.h
+++ public/inetchannelinfo.h
@@ -59,7 +59,6 @@ public:
 	virtual const netadr_t &GetRemoteAddress( void ) const = 0;
 	virtual float		GetTime( void ) const = 0;	// current net time
 	virtual float		GetTimeConnected( void ) const = 0;	// get connection time in seconds
-	virtual int			GetBufferSize( void ) const = 0;	// netchannel packet history size
 	virtual int			GetDataRate( void ) const = 0; // send data rate in byte/sec
 	
 	virtual bool		IsLocalHost( void ) const = 0;	// true if localhost
