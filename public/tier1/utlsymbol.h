9158ae233224953c7b851f3ef41e6f0ea52b3f72
Kit o' Rifty <kitrifty@gmail.com>
2025-11-03 11:17:37 -0800

    Clarify use of overload CUtlSymbolTable::Find (#352)
    allows compiling C++20 on Windows

--- public/tier1/utlsymbol.h
+++ public/tier1/utlsymbol.h
@@ -105,7 +105,7 @@ public:
 	
 	inline bool HasElement( const char* pStr ) const
 	{
-		return Find( pStr ) != UTL_INVAL_SYMBOL;
+		return (UtlSymId_t)(Find( pStr )) != UTL_INVAL_SYMBOL;
 	}
 
 	// Remove all symbols in the table.
