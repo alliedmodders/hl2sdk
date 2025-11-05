c1037671679aacb45865333fe8923d9eb15b8933
GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>
2025-11-05 14:16:28 +0300

    Update name of the new method

--- public/irecipientfilter.h
+++ public/irecipientfilter.h
@@ -27,7 +27,8 @@ public:
 	virtual bool	IsInitMessage( void ) const = 0;
 
 	virtual const CPlayerBitVec &GetRecipients( void ) const = 0;
-	virtual CPlayerSlot GetPredictedByPlayerSlot( void ) const = 0;
+	virtual CPlayerSlot GetPredictedPlayerSlot( void ) const = 0;
 };
 
 #endif // IRECIPIENTFILTER_H
+
