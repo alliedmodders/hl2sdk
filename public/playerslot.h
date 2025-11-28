9aaac5262c6f2771caeb9ddb8c8ed7a435306077
GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>
2025-11-28 19:04:30 +0300

    Add few QoL methods to CPlayerSlot

--- public/playerslot.h
+++ public/playerslot.h
@@ -5,24 +5,19 @@
 #pragma once
 #endif
 
+#include "const.h"
+
 class CPlayerSlot
 {
 public:
-	CPlayerSlot(int slot) : m_Data(slot)
-	{
-	}
+	CPlayerSlot( int slot ) : m_Data( slot ) {}
 
-	int Get() const
-	{
-		return m_Data;
-	}
+	void Invalidate() { m_Data = -1; }
+	bool IsValid() const { return m_Data >= 0 && m_Data < ABSOLUTE_PLAYER_LIMIT; }
+	int Get() const { return m_Data; }
 
-	bool operator==(const CPlayerSlot &other) const {
-		return other.m_Data == m_Data;
-	}
-	bool operator!=(const CPlayerSlot &other) const {
-		return other.m_Data != m_Data;
-	}
+	bool operator==( const CPlayerSlot &other ) const { return other.m_Data == m_Data; }
+	bool operator!=( const CPlayerSlot &other ) const { return other.m_Data != m_Data; }
 
 private:
 	int m_Data;
