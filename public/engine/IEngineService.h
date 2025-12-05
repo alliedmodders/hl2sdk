dab138af32c3bc0795bbb3a24f706f60d966b7f0
GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>
2025-12-05 13:34:57 +0300

    Small method name adjustment

--- public/engine/IEngineService.h
+++ public/engine/IEngineService.h
@@ -96,7 +96,7 @@ public:
 	virtual void		RegisterPrerequisite( IPrerequisite * ) = 0;
 	
 	// Same methods as ILocalize
-	virtual LocalizeStringIndex_t FindIndex(const char *tokenName) = 0;
+	virtual LocalizeStringIndex_t FindLocalizeTokenIndex(const char *tokenName) = 0;
 
 	// Same methods as IVEngineServer2 
 	virtual void		SetFrameTimeAmnesty( const char *amnesty, int, float frametime ) = 0;
