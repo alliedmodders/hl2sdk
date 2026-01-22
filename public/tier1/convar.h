e23b6075effe41c77406999e53197ffc2229841e
GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>
2026-01-22 16:35:15 +0300

    Update ConVarValueInfo_t & ConVarData

--- public/tier1/convar.h
+++ public/tier1/convar.h
@@ -626,6 +626,8 @@ using FnTypedFilterCallbackProvider_t = bool(*)(CConVar<T> *cvar, CSplitScreenSl
 using FnGenericFilterCallback_t = bool(*)(ConVarRefAbstract *ref, CSplitScreenSlot nSlot, const CVValue_t *pNewValue, const CVValue_t *pOldValue);
 using FnGenericFilterCallbackProvider_t = bool(*)(ConVarRefAbstract *ref, CSplitScreenSlot nSlot, const CVValue_t *pNewValue, const CVValue_t *pOldValue, void *__unk01, FnGenericFilterCallback_t cb);
 
+using FnCustomData_t = void *(*)();
+
 struct ConVarValueInfo_t
 {
 	ConVarValueInfo_t( EConVarType type = EConVarType_Invalid ) :
@@ -640,6 +642,7 @@ struct ConVarValueInfo_t
 		m_fnCallBack( nullptr ),
 		m_fnProviderFilterCallBack( nullptr ),
 		m_fnFilterCallBack( nullptr ),
+		m_fnCustomData( nullptr ),
 		m_eVarType( type ),
 		m_CompletionCallBack()
 	{}
@@ -714,6 +717,12 @@ public:
 
 	FnGenericFilterCallbackProvider_t m_fnProviderFilterCallBack;
 	FnGenericFilterCallback_t m_fnFilterCallBack;
+
+	// AMNOTE: Currently the only usage is lb_debug_tiles, lb_debug_silhouette and sc_visualize_sceneobjects
+	// which holds a reference to a enum schema binding, it's a string type under the hood and is converted from int
+	// to a enum value string via the change callbacks.
+	// So not sure if this is a concrete enum binding prop or any data prop.
+	FnCustomData_t m_fnCustomData;
 
 	EConVarType m_eVarType;
 
@@ -896,7 +905,10 @@ public:
 		m_nFlags = FCVAR_REFERENCE;
 		m_iCallbackIndex = 0;
 		m_iFilterCBIndex = 0;
+		m_iCompletionCBIndex = 0;
 		m_GameInfoFlags = 0;
+		m_UserInfoByteIndex = 0;
+		m_fnCustomData = nullptr;
 	}
 
 	const char *GetName( void ) const { return m_pszName; }
@@ -927,6 +939,8 @@ public:
 		Assert( m_eVarType != EConVarType_Invalid );
 		return GetCvarTypeTraits( m_eVarType );
 	}
+
+	FnCustomData_t GetCustomDataFn() const { return m_fnCustomData; }
 
 	int GetDataByteSize() const { return TypeTraits()->m_ByteSize; }
 	bool IsPrimitiveType() const { return TypeTraits()->m_IsPrimitive; }
@@ -1011,6 +1025,9 @@ private:
 
 	int m_GameInfoFlags;
 	int m_UserInfoByteIndex;
+
+	// Copied directly as is from ConVarValueInfo_t
+	FnCustomData_t m_fnCustomData;
 
 	// At convar registration this is trimmed to better match convar type being used
 	// or if it was initialized as EConVarType_Invalid it would be of this size
