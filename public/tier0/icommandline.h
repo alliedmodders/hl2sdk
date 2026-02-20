2446d6d3073137813af6d566c45027c5f01883aa
GAMMACASE <31375974+GAMMACASE@users.noreply.github.com>
2026-02-20 17:02:14 +0300

    Update ICommandLine

--- public/tier0/icommandline.h
+++ public/tier0/icommandline.h
@@ -31,7 +31,7 @@ public:
 	
 	// Gets at particular parameters
 	virtual int			ParmCount() const = 0;
-	virtual int			FindParm( CUtlStringToken param ) const = 0;	// Returns 0 if not found.
+	virtual int			FindParm( CUtlStringToken param, int startIdx = -1 ) const = 0;	// Returns 0 if not found.
 	virtual const char* GetParm( int nIndex ) const = 0;
 
 	// Returns the argument after the one specified, or the default if not found
@@ -47,7 +47,20 @@ public:
 	// Returns true if there's atleast one parm available
 	virtual bool		HasParms( void ) const = 0;
 
-	virtual const char *GetUnkString() = 0;
+	virtual const char *GetParmBuffer() = 0;
+
+	virtual	const char *CheckParmUnrestricted( CUtlStringToken param, const char **ppszValue = 0 ) const = 0;
+	virtual bool		HasParmUnrestricted( CUtlStringToken param ) const = 0;
+
+	virtual int			FindParmUnrestricted( CUtlStringToken param, int startIdx = -1 ) const = 0;	// Returns 0 if not found.
+
+	// Returns the argument after the one specified, or the default if not found
+	virtual const char *ParmValueUnrestricted( CUtlStringToken param, const char *pDefaultVal = 0 ) const = 0;
+	virtual int			ParmValueUnrestricted( CUtlStringToken param, int nDefaultVal ) const = 0;
+	virtual float		ParmValueUnrestricted( CUtlStringToken param, float flDefaultVal ) const = 0;
+	virtual bool		ParmValueUnrestricted( CUtlStringToken param, const char *pDefaultVal, CBufferString *bufOut ) = 0;
+
+	virtual				~ICommandLine() = 0;
 };
 
 //-----------------------------------------------------------------------------
