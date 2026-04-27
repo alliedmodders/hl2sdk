2a8f3d9236b9034d9dc75523bffb43fd7aaf61d8
Nicholas Hastings <nshastings@gmail.com>
2026-04-27 01:43:12 +0000

    Compatibility fixes for Clang 22

--- public/tier0/threadtools.h
+++ public/tier0/threadtools.h
@@ -983,7 +983,7 @@ private:
 class TFRWL_ALIGN TT_CLASS CThreadSpinRWLock
 {
 public:
-	CThreadSpinRWLock()	{ COMPILE_TIME_ASSERT( sizeof( LockInfo_t ) == sizeof( int64 ) ); Assert( (int)this % 8 == 0 ); memset( this, 0, sizeof( *this ) ); }
+	CThreadSpinRWLock()	{ COMPILE_TIME_ASSERT( sizeof( LockInfo_t ) == sizeof( int64 ) ); Assert( (int)this % 8 == 0 ); memset( (void *)this, 0, sizeof( *this ) ); }
 
 	bool TryLockForWrite();
 	bool TryLockForRead();
