ebbe99c8da05735d4f19cf54508879b642043171
Dots <dotvnl@gmail.com>
2026-07-12 21:20:17 +0300

    revert comment

--- public/tier1/utlmap.h
+++ public/tier1/utlmap.h
@@ -201,11 +201,7 @@ protected:
 
 //-----------------------------------------------------------------------------
 
-//-----------------------------------------------------------------------------
-// CUtlOrderedMap: the Source2 name for the red-black-tree ordered map.
-// Storage is identical to CUtlMap.
-// The argument order matches the engine: < Key, Type, LessFunc, Index >
-//-----------------------------------------------------------------------------
+// AMNOTE: Currently a stub over CUtlMap, needs a complete implementation
 template <typename K, typename T, typename L = CDefLess<K>, typename I = int>
 class CUtlOrderedMap : public CUtlMap<K, T, I, L>
 {
