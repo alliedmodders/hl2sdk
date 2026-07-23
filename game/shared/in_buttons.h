a91bd37c182b2280eef5af85c3faa9bdc2b1c709
Michal Přikryl <slynx@funplay.pro>
2026-07-23 13:02:54 +0200

    fix: use 64-bit shifts in InputBitMask_t

--- game/shared/in_buttons.h
+++ game/shared/in_buttons.h
@@ -11,27 +11,27 @@ enum InputBitMask_t : int64
 	IN_NONE	= 0,
 	IN_ALL	= -1,
 
-	IN_ATTACK			= (1 << 0),
-	IN_JUMP				= (1 << 1),
-	IN_DUCK				= (1 << 2),
-	IN_FORWARD			= (1 << 3),
-	IN_BACK				= (1 << 4),
-	IN_USE				= (1 << 5),
-	IN_TURNLEFT			= (1 << 7),
-	IN_TURNRIGHT		= (1 << 8),
-	IN_MOVELEFT			= (1 << 9),
-	IN_MOVERIGHT		= (1 << 10),
-	IN_ATTACK2			= (1 << 11),
-	IN_RELOAD			= (1 << 13),
-	IN_SPEED			= (1 << 16), // Player is holding the speed key
-	IN_JOYAUTOSPRINT	= (1 << 17),
+	IN_ATTACK			= (1ll << 0),
+	IN_JUMP				= (1ll << 1),
+	IN_DUCK				= (1ll << 2),
+	IN_FORWARD			= (1ll << 3),
+	IN_BACK				= (1ll << 4),
+	IN_USE				= (1ll << 5),
+	IN_TURNLEFT			= (1ll << 7),
+	IN_TURNRIGHT		= (1ll << 8),
+	IN_MOVELEFT			= (1ll << 9),
+	IN_MOVERIGHT		= (1ll << 10),
+	IN_ATTACK2			= (1ll << 11),
+	IN_RELOAD			= (1ll << 13),
+	IN_SPEED			= (1ll << 16), // Player is holding the speed key
+	IN_JOYAUTOSPRINT	= (1ll << 17),
 
-	IN_FIRST_MOD_SPECIFIC_BIT = (1 << 32),
+	IN_FIRST_MOD_SPECIFIC_BIT = (1ll << 32),
 
 	IN_USEORRELOAD		= IN_FIRST_MOD_SPECIFIC_BIT,
-	IN_SCORE			= (1 << 33), // Used by client.dll for when scoreboard is held down
-	IN_ZOOM				= (1 << 34), // Zoom key for HUD zoom
-	IN_LOOK_AT_WEAPON	= (1 << 35)
+	IN_SCORE			= (1ll << 33), // Used by client.dll for when scoreboard is held down
+	IN_ZOOM				= (1ll << 34), // Zoom key for HUD zoom
+	IN_LOOK_AT_WEAPON	= (1ll << 35)
 };
 
 #endif // IN_BUTTONS_H
